76c6f260657dcc5224bb80f1802b11a004dfcf81
Benjamin Bettan <benjamin.bettan@gmail.com>
2019-12-09 12:15:35 +0100

    Implementation etape 1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # webAppOneNoteLike
+
+Etape 0
+A cette étape je peux faire localhost:8080 sur un navigateur 
+	-> This application has no explicit mapping for /error, so you are seeing this as a fallback.
+
+Etape 1
+Ajout de code dans la classe HomeControler
+localhost:8080 -> Hello toto
+
--- src/main/java/com/bbe/webappOneNoteLike/web/HomeController.java
+++ src/main/java/com/bbe/webappOneNoteLike/web/HomeController.java
@@ -1,5 +1,17 @@
 package com.bbe.webappOneNoteLike.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+	@Value("${conference.name:toto}")
+	private String userName;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello " + userName;
+	}
 }
